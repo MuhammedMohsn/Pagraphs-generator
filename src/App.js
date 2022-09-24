f880c6a995d427d43efc29ea70cb87b7a45c9624
muhammed mohsen <muhammedmohsen1111@gmail.com>
2022-09-24 08:49:17 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
-// import logo from './logo.svg';
 import './App.css';
-import { Fragment,useState } from 'react';
+import { Fragment, useState } from 'react';
 import Data from './data'
 
 function App() {
-  let [count,setcount]=useState(0)
-  let [text,settext] = useState([])
-  const submithandler=(e)=>{
-  e.preventDefault();
-  let amount=parseInt(count)
-  if (amount>8){
-     amount=8
-  }
-  if(amount<=0){
-    amount=0
-  }
- 
-  settext(Data.slice(0,amount))
-
-
+  let [count, setcount] = useState(0)
+  let [text, settext] = useState([])
+  // submit handler to slice paragraphs according to input value
+  const submithandler = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count)
+    if (amount > 8) {
+      amount = 8
+    }
+    if (amount <= 0) {
+      amount = 0
+    }
+    settext(Data.slice(0, amount))
   }
   return (
-    <Fragment> 
-   <div className="container"> <h1>TIRED OF BORING LOREM IPSUM?</h1>
+    <Fragment>
+      <div className="container"> <h1>TIRED OF BORING LOREM IPSUM?</h1>
 
-   <form onSubmit={(e)=>submithandler(e)}>
-   <label htmlFor="paragraphs"> Paragraphs : </label>
-    <input type="number" value={count} onChange={(e)=>setcount(e.target.value)} id="paragraphs"/>
-    <button  className="generate">Generate</button>
- 
-   </form>
-  <article> {text.map(item=>{
-    return (<p key={Math.random()} className="text">{item}</p>) })}</article>
-    
-  
-   
-   </div>
-  </Fragment>
+        <form onSubmit={(e) => submithandler(e)}>
+          <label htmlFor="paragraphs"> Paragraphs : </label>
+          <input type="number" value={count} onChange={(e) => setcount(e.target.value)} id="paragraphs" />
+          <button className="generate">Generate</button></form>
+
+        <article> {text.map(item => {
+          return (<p key={Math.random()} className="text">{item}</p>)
+        })}</article>
+
+      </div>
+    </Fragment>
   );
-   
+
 }
 
 export default App;
